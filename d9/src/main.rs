6cdb2c500e5b9bd1f7269341a75b280e57a969f9
Jono Prest <jjprest@gmail.com>
2023-11-02 19:03:22 +0200

    Wip wrong simulation

--- d9/src/main.rs
+++ d9/src/main.rs
@@ -43,7 +43,7 @@ mod test {
         rope_simulator::{
             Coordinate,
             Direction::{Down, Left, Right, Up},
-            Motion, RopeMap,
+            KnottedRopeMap, Motion, RopeMap,
         },
     };
 
@@ -90,4 +90,139 @@ mod test {
 
         assert_eq!(13, no_unique_tail_positions);
     }
+
+    #[test]
+    fn validated_correct_part_1_answer() {
+        let input_file = File::open("./my_input.txt").expect("opening file");
+        let parsed = input_parser::parse_input(&input_file).expect("parsing file");
+
+        let mut rope_map = RopeMap::init();
+
+        for motion in parsed {
+            rope_map.move_rope_n_times(motion.direction, motion.amount);
+        }
+
+        let no_unique_tail_positions = rope_map
+            .path
+            .into_iter()
+            .map(|rco| rco.tail)
+            .collect::<HashSet<Coordinate>>()
+            .iter()
+            .count();
+
+        assert_eq!(6563, no_unique_tail_positions);
+    }
+
+    #[test]
+    fn given_example_1_part_2() {
+        let input_file = File::open("./example.txt").expect("opening file");
+        let parsed = input_parser::parse_input(&input_file).expect("parsing file");
+
+        let mut knotted_rope_map = KnottedRopeMap::init(10);
+
+        for motion in parsed {
+            knotted_rope_map.move_rope_n_times(motion.direction, motion.amount);
+        }
+
+        let mut third_knot_path: Vec<Coordinate> = Vec::new();
+        for knotted_rope_co in knotted_rope_map.path.iter() {
+            let tail = &knotted_rope_co.knots[2].head;
+            third_knot_path.push(tail.clone());
+        }
+
+        assert_eq!(third_knot_path[0].x, 0, "0 move x should be 0");
+        assert_eq!(third_knot_path[1].x, 0, "1 move x should be 0");
+        assert_eq!(third_knot_path[2].x, 0, "2 move x should be 0");
+        assert_eq!(third_knot_path[3].x, 1, "3 move x should be 1");
+        assert_eq!(third_knot_path[4].x, 2, "4 move x should be 2");
+        assert_eq!(third_knot_path[5].x, 2, "5 move x shoud be 2");
+        assert_eq!(third_knot_path[6].x, 3, "6 move x shoud be 3");
+
+        assert_eq!(third_knot_path[0].y, 0, "0 move y should be 0");
+        assert_eq!(third_knot_path[1].y, 0, "1 move y should be 0");
+        assert_eq!(third_knot_path[2].y, 0, "2 move y should be 0");
+        assert_eq!(third_knot_path[3].y, 0, "3 move y should be 0");
+        assert_eq!(third_knot_path[4].y, 0, "4 move y should be 0");
+        assert_eq!(third_knot_path[5].y, 0, "5 move y shoud be 0");
+        assert_eq!(third_knot_path[6].y, 1, "6 move y shoud be 1");
+    }
+
+    #[test]
+    fn given_example_2_part_2() {
+        let input_file = File::open("./example2.txt").expect("opening file");
+        let parsed = input_parser::parse_input(&input_file).expect("parsing file");
+
+        let mut knotted_rope_map = KnottedRopeMap::init(9);
+
+        let mut motions_iter = parsed.into_iter();
+
+        let motion = motions_iter.next().unwrap();
+        knotted_rope_map.move_rope_n_times(motion.direction.clone(), motion.amount);
+
+        assert_eq!(Right, motion.direction);
+        assert_eq!(5, motion.amount);
+        assert_eq!(0, knotted_rope_map.get_last_co().get_tail().tail.x);
+        assert_eq!(0, knotted_rope_map.get_last_co().get_tail().tail.y);
+
+        let motion = motions_iter.next().unwrap();
+        knotted_rope_map.move_rope_n_times(motion.direction.clone(), motion.amount);
+
+        assert_eq!(Up, motion.direction);
+        assert_eq!(8, motion.amount);
+        assert_eq!(0, knotted_rope_map.get_last_co().get_tail().tail.x);
+        assert_eq!(0, knotted_rope_map.get_last_co().get_tail().tail.y);
+
+        let motion = motions_iter.next().unwrap();
+        knotted_rope_map.move_rope_n_times(motion.direction.clone(), motion.amount);
+
+        assert_eq!(Left, motion.direction);
+        assert_eq!(8, motion.amount);
+        assert_eq!(1, knotted_rope_map.get_last_co().get_tail().tail.x);
+        assert_eq!(3, knotted_rope_map.get_last_co().get_tail().tail.y);
+        // for motion in parsed {
+        // }
+
+        let no_unique_tail_positions = knotted_rope_map
+            .path
+            .iter()
+            .map(|rco| rco.get_tail().tail.clone())
+            .collect::<HashSet<Coordinate>>()
+            .iter()
+            .count();
+
+        assert_eq!(36, no_unique_tail_positions);
+    }
+
+    // #[test]
+    // fn given_example_part_2_chunk() {
+    //     let input_file = File::open("./example2.txt").expect("opening file");
+    //     let parsed = input_parser::parse_input(&input_file).expect("parsing file");
+    //
+    //     let mut knotted_rope_map = KnottedRopeMap::init(10);
+    //
+    //     for motion in parsed[0..2].into_iter() {
+    //         knotted_rope_map.move_rope_n_times(motion.direction.clone(), motion.amount);
+    //     }
+    //
+    //     // println!(
+    //     //     "{:?}",
+    //     //     knotted_rope_map
+    //     //         .path
+    //     //         .iter()
+    //     //         .map(|kr| kr.get_tail())
+    //     //         .collect::<Vec<_>>()
+    //     // );
+    //
+    //     let mut hash_set: HashSet<Coordinate> = HashSet::new();
+    //     for (i, knotted_rope_co) in knotted_rope_map.path.iter().enumerate() {
+    //         let tail = &knotted_rope_co.knots[4].head;
+    //
+    //         let new = hash_set.insert(tail.clone());
+    //         println!("{} {:?}", i, tail);
+    //     }
+    //
+    //     let no_unique_tail_positions = hash_set.iter().count();
+    //
+    //     assert_eq!(0, no_unique_tail_positions);
+    // }
 }
--- d9/src/rope_simulator.rs
+++ d9/src/rope_simulator.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Direction {
     Up,
     Down,
@@ -6,7 +6,7 @@ pub enum Direction {
     Right,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Motion {
     pub direction: Direction,
     pub amount: i32,
@@ -20,8 +20,8 @@ impl Motion {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Coordinate {
-    x: i32,
-    y: i32,
+    pub x: i32,
+    pub y: i32,
 }
 
 impl Coordinate {
@@ -49,9 +49,13 @@ impl Coordinate {
     fn is_within_1_range(&self, cmp: &Coordinate) -> bool {
         Self::val_is_within_1_range(self.x, cmp.x) && Self::val_is_within_1_range(self.y, cmp.y)
     }
+
+    fn is_linear_movement(&self, cmp: &Coordinate) -> bool {
+        self.x == cmp.x || self.y == cmp.y
+    }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct RopeCoordinate {
     pub head: Coordinate,
     pub tail: Coordinate,
@@ -66,44 +70,47 @@ impl RopeCoordinate {
         Self::new(Coordinate::init(), Coordinate::init())
     }
 
-    fn move_rope(&self, direction: &Direction) -> Self {
-        match (self, direction) {
-            (rope_co, direction)
+    fn move_head_to_co(&self, next_head_co: Coordinate) -> Self {
+        match (self, next_head_co) {
+            (rope_co, next_head_co)
                 //Only move the head in the the case
                 //where the move destination is within 1
                 //range of tail
-                if rope_co.head.move_direction(&direction).is_within_1_range(&rope_co.tail) =>
+                if next_head_co.is_within_1_range(&rope_co.tail)  =>
             {
               Self::new(
-                    rope_co.head.move_direction(direction),
+                    next_head_co,
                     rope_co.tail.clone(),
                 )
             }
             //In the case where they are in the same row and moving left or right
-            //Or same column moving up or down
-            //both head and tail move in that direction
-            (rope_co, direction)
-                if (rope_co.head.y == rope_co.tail.y
-                    && matches!(direction, Direction::Left | Direction::Right))
-                ||(rope_co.head.x == rope_co.tail.x
-                    && matches!(direction, Direction::Up | Direction::Down) ) =>
+            //both head and tail move in that direction by the same amount
+            (rope_co, next_head_co)
+                if !rope_co.head.is_linear_movement(&next_head_co) =>
             {
-                 Self::new(
-                    rope_co.head.move_direction(direction),
-                    rope_co.tail.move_direction(direction),
-                )
+                 Self::new(next_head_co.clone(),
+                        Coordinate::new(
+                            rope_co.tail.x + (next_head_co.x - rope_co.head.x),
+                            rope_co.tail.y + (next_head_co.y - rope_co.head.y)
+                        )
+                    )
             }
             //In the default case where head and tail are not on the same column or row
             //and the head move is not within 1 of the tail, head moves and the tail occupies
             //the space of the head was previously in
-            (rope_co, direction) => {
+            (rope_co, next_head_co) => {
                  Self::new(
-                    rope_co.head.move_direction(direction),
+                    next_head_co,
                     rope_co.head.clone(),
                 )
 
             }
         }
+    }
+
+    fn move_rope(&self, direction: &Direction) -> Self {
+        let next_head_co = self.head.move_direction(direction);
+        self.move_head_to_co(next_head_co)
     }
 }
 
@@ -140,10 +147,80 @@ impl RopeMap {
     }
 }
 
+#[derive(Debug)]
+pub struct KnottedRope {
+    pub knots: RopeCoordinates,
+}
+
+impl KnottedRope {
+    fn new(knots: RopeCoordinates) -> Self {
+        KnottedRope { knots }
+    }
+    pub fn init(size: usize) -> Self {
+        let knots = vec![RopeCoordinate::init(); size];
+        Self::new(knots)
+    }
+
+    pub fn get_head(&self) -> &RopeCoordinate {
+        self.knots.first().expect("Should be a knot at the head")
+    }
+    pub fn get_tail(&self) -> &RopeCoordinate {
+        self.knots.last().expect("Should be a knot at the tail")
+    }
+
+    pub fn move_rope(&self, direction: &Direction) -> Self {
+        let head_knot = self.get_head();
+        let mut next_head_co = head_knot.head.move_direction(direction);
+        let mut next_knot_cos = Vec::new();
+
+        for knot in &self.knots {
+            let next_knot_co = knot.move_head_to_co(next_head_co);
+            next_head_co = next_knot_co.tail.clone();
+            next_knot_cos.push(next_knot_co);
+        }
+
+        Self::new(next_knot_cos)
+    }
+}
+
+type KnottedRopes = Vec<KnottedRope>;
+
+#[derive(Debug)]
+pub struct KnottedRopeMap {
+    pub path: KnottedRopes,
+}
+
+impl KnottedRopeMap {
+    pub fn init(size: usize) -> Self {
+        let init_coordinate = KnottedRope::init(size);
+        KnottedRopeMap {
+            path: vec![init_coordinate],
+        }
+    }
+
+    pub fn get_last_co(&self) -> &KnottedRope {
+        self.path
+            .last()
+            .expect("Should always be at least 1 coordinate")
+    }
+
+    fn move_rope(&mut self, direction: &Direction) {
+        let last_co = self.get_last_co();
+        let next_co = last_co.move_rope(direction);
+        self.path.push(next_co);
+    }
+
+    pub fn move_rope_n_times(&mut self, direction: Direction, n: i32) {
+        for _ in 0..n {
+            self.move_rope(&direction);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use Direction::{Down, Left, Right, Up};
+    use Direction::{Down, Right, Up};
 
     #[test]
     fn validate_within_one_range_path() {
@@ -238,5 +315,21 @@ mod test {
         let co_end = RopeCoordinate::new(head_end, tail_end);
 
         assert_eq!(co_end, co_start.move_rope(&Down));
+    }
+
+    #[test]
+    fn knot_jump_motion() {
+        let head_start = Coordinate::new(2, 0);
+        let tail_start = Coordinate::new(1, 0);
+        let co_start = RopeCoordinate::new(head_start, tail_start);
+
+        let next_co_head = Coordinate::new(3, 1);
+        let next_co_tail = Coordinate::new(2, 1);
+        let co_end = RopeCoordinate::new(next_co_head.clone(), next_co_tail);
+
+        assert!(!co_start.head.is_linear_movement(&next_co_head));
+        assert!(!next_co_head.is_within_1_range(&co_start.tail));
+        assert_eq!(co_end, co_start.move_head_to_co(next_co_head));
+        //                 && next_head_co.is_within_1_range(&rope_co.tail)
     }
 }
